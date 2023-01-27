fbfff8eb7c602ea0d74b04a99457a3604e9dd199
dominic-vancleave <54820009+dominic-vancleave@users.noreply.github.com>
2023-01-27 11:54:47 -0500

    Update nebraska_tier2_onmap.py

--- nebraska_tier2_onmap.py
+++ nebraska_tier2_onmap.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Oct 12 17:39:26 2022
-
-@author: domin
+Last Updated on Fri Jan 27 2023
+@author: Dominic Van Cleave-Schottland
+"""
+"""
+The code below taks in the geographic location data of Tier II tanks in Nebraska and outputs a 
+map displaying these tanks within Nebraska. Each dot on the map represents one AST. The site
+used for creating and displaying a map of Nebraska is:
+https://www.openstreetmap.org/
 """
 #%% Importing Libraries
 import pandas as pd
@@ -14,8 +20,12 @@ neb_tier2_data = pd.read_csv('coordinates.csv')
 latitudes_tier2 = neb_tier2_data['LAT'].tolist()
 longitudes_tier2_pos = neb_tier2_data['LON'].tolist()
 
+# For some reason, the longitudes were given as positive numbers, so this
+# brief code just negates all numbers.
 longitudes_tier2 = [ -x for x in longitudes_tier2_pos]
 
+# Some data was also out of the geographic boundaries of Nebraska, and needed
+# to be deleted. 
 for i in range(0, len(latitudes_tier2)-1):
     if latitudes_tier2[i] > 100:
         del latitudes_tier2[i]
@@ -26,6 +36,7 @@ neb_boundary = (-104.2, -95.1,
                   39.9,  43.1)
 
 neb_map = plt.imread('Nebraska_map.png')
+
 #%% Plotting Tank Coords
 fig = plt.figure(figsize=(25,12), dpi=100)
 ax = fig.add_subplot(111)
